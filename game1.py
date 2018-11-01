6a5a5e43fec85189bd021c9e98d6efed0276986d
James Evans <jaevans@users.sf.net>
2018-11-01 22:53:08 +0000

    2018-11-1 session

--- game1.py
+++ game1.py
@@ -4,7 +4,6 @@ import math
 class TurtleActor(object):
     def __init__(self, *args, **kwargs):
         self.__dict__['_actor'] = Actor(*args, **kwargs)
-        self.velocity = 0
         
     def __getattr__(self, attr):
         if attr in self.__dict__:
@@ -33,6 +32,12 @@ class TurtleActor(object):
     def turnright(self, angle):
         self._actor.angle -= angle
 
+        
+class TankActor(TurtleActor):
+    def __init__(self, color):
+        super().__init__(color + '_tank')
+        self.velocity = 0
+
     def set_velocity(self, value):
         self.velocity = value
 
@@ -40,40 +45,65 @@ class TurtleActor(object):
         self.forward(self.velocity)
         if self.velocity > 0:
             self.velocity /= 1.1
-        
-class TankActor(TurtleActor):
-    def __init__(self, color):
-        super().__init__(color + '_tank')
+        if self.velocity < 0:
+            self.velocity += .25
+        if self.left > WIDTH:
+            self.right = 0
+        if self.right < 0:
+            self.left = WIDTH
+        if self.top > HEIGHT:
+            self.bottom = 0
+        if self.bottom < 0:
+            self.top = HEIGHT
+
+class BulletActor(TurtleActor):
+    def __init__ (self,color):
+        super().__init__(color + '_bullet')
 
 red_tank = TankActor('red')
-red_tank.topright = 0, 10
+red_tank.topleft = 10, 10
 blue_tank = TankActor('blue')
-blue_tank.topright = 200, 200
+blue_tank.bottomright = 490, 490
+blue_tank.turnleft (180)
 WIDTH = 500
 HEIGHT = 500
-
+red_bullet = BulletActor('red')
+red_bullet.bottomright= 200,200
+blue_bullet = BulletActor('blue')
+blue_bullet.bottomright = 300,300
 velocity = 0
+
 def draw():
     screen.clear()
     screen.fill('white')
+    red_bullet.draw()
+    blue_bullet.draw()
     red_tank.draw()
     blue_tank.draw()
+
 def update():
     red_tank.move()
-    if red_tank.left > WIDTH:
-        red_tank.right = 0
-    if red_tank.right < 0:
-        red_tank.left = WIDTH
-    if red_tank.top > HEIGHT:
-        red_tank.bottom = 0
-    if red_tank.bottom < 0:
-        red_tank.top = HEIGHT
-        
-def on_key_down(key):
-    if key == keys.W :
-        red_tank.velocity += 5
-    if key == keys.A :
-        red_tank.turnleft(15)
-    if key == keys.D :
-        red_tank.turnright(15)
+    if keyboard[keys.W]:
+        if red_tank.velocity <= 10:
+            red_tank.velocity += .5
+    if keyboard[keys.S]:
+        if red_tank.velocity >= -5:
+         red_tank.velocity -= .5
+    if keyboard[keys.A]:
+        red_tank.turnleft(5)
+    if keyboard[keys.D]:
+        red_tank.turnright(5)
+
+    blue_tank.move()
+    if keyboard[keys.UP]:
+        if blue_tank.velocity <= 10:
+            blue_tank.velocity += .5
+    if keyboard[keys.DOWN]:
+        if blue_tank.velocity >= -5:
+         blue_tank.velocity -= .5
+    if keyboard[keys.LEFT]:
+        blue_tank.turnleft(5)
+    if keyboard[keys.RIGHT]:
+        blue_tank.turnright(5)
+
 pgzrun.go()
