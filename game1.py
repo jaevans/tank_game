7305eeb46488697c5d66663ae9297a8d55ab121e
James Evans <jaevans@users.sf.net>
2018-11-16 01:31:39 +0000

    2018-11-15

--- game1.py
+++ game1.py
@@ -1,5 +1,6 @@
 import pgzrun
 import math
+import pygame
 
 class TurtleActor(object):
     def __init__(self, *args, **kwargs):
@@ -38,7 +39,10 @@ class TankActor(TurtleActor):
         super().__init__(color + '_tank')
         self.velocity = 0
         self.bullet = None
-
+        self.color = color
+        self.canfire = True
+        self.hp = 10
+        
     def set_velocity(self, value):
         self.velocity = value
 
@@ -60,6 +64,17 @@ class TankActor(TurtleActor):
             self.bullet.move()
             if not self.bullet.onscreen():
                 self.bullet = None
+
+    def fire(self):
+        if self.bullet is None and self.canfire == True:
+            self.bullet = BulletActor(self.color, self.angle)
+            self.bullet.center = self.center
+            self.canfire = False
+            clock.schedule(self.resetfire , 1.0)
+
+    def resetfire(self):
+        self.canfire = True
+
 
 class BulletActor(TurtleActor):
     def __init__ (self,color,angle):
@@ -89,48 +104,47 @@ def draw():
     if blue_tank.bullet is not None:
         blue_tank.bullet.draw()
     red_tank.draw()
-    draw.rect(screen.surface, red_tank._rect, width=2)
+    #draw.rect(screen.surface, red_tank._rect, width=2)
     #screen.rect(, 'white')
     blue_tank.draw()
 
 def update():
     red_tank.move()
     if keyboard[keys.W]:
-        if red_tank.velocity <= 10:
+        if red_tank.velocity <= 5:
             red_tank.velocity += .5
     if keyboard[keys.S]:
         if red_tank.velocity >= -5:
          red_tank.velocity -= .5
     if keyboard[keys.A]:
-        red_tank.turnleft(5)
+        red_tank.turnleft(2.5)
     if keyboard[keys.D]:
-        red_tank.turnright(5)
-    if keyboard[keys.E]:
-        if red_tank.bullet is None:
-            red_tank.bullet = BulletActor('red', red_tank.angle)
-            red_tank.bullet.center = red_tank.center
+        red_tank.turnright(2.5)
     if red_tank.bullet is not None:
-        if red_tank.bullet._rect.colliderect(blue_tank._rect):
+        rdist = (blue_tank.x - red_tank.bullet.x)**2 + (blue_tank.y - red_tank.bullet.y)**2
+        if rdist < 30**2:
             red_tank.bullet = None
     if blue_tank.bullet is not None:
-        if blue_tank.bullet._rect.colliderect(red_tank._rect):
-            blue_tank.bullet = None
+        bdist = (red_tank.x - blue_tank.bullet.x)**2 + (red_tank.y - blue_tank.bullet.y)**2
+        if bdist < 30**2:
+           blue_tank.bullet = None
 
     blue_tank.move()
     if keyboard[keys.UP]:
-        if blue_tank.velocity <= 10:
+        if blue_tank.velocity <= 5:
             blue_tank.velocity += .5
     if keyboard[keys.DOWN]:
         if blue_tank.velocity >= -5:
          blue_tank.velocity -= .5
     if keyboard[keys.LEFT]:
-        blue_tank.turnleft(5)
+        blue_tank.turnleft(2.5)
     if keyboard[keys.RIGHT]:
-        blue_tank.turnright(5)
-    if keyboard[keys.M]:
-        if blue_tank.bullet is None:
-            blue_tank.bullet = BulletActor('blue', blue_tank.angle)
-            blue_tank.bullet.center = blue_tank.center
+        blue_tank.turnright(2.5)
         
+def on_key_down(key):
+    if key == keys.E:
+        red_tank.fire()
+    if key == keys.M:
+        blue_tank.fire()
 
 pgzrun.go()
